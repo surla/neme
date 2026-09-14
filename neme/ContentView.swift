fce1fbe38ec3552855b56c535481083e7dcc2222
Joseph Surla <josephsurla@gmail.com>
2026-09-14 15:30:41 -0700

    chore: initial Xcode project setup

---
+++ neme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  neme
+//
+//  Created by surla on 9/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ neme/nemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nemeApp.swift
+//  neme
+//
+//  Created by surla on 9/14/26.
+//
+
+import SwiftUI
+
+@main
+struct nemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
